70a10e336ab2b1d3c33e7f9ef5bb2f708d2ce60d
Jim Ward <seker@uwyo.edu>
2017-11-10 13:36:01 -0700

    update to support wear, API 27, and studio 3.0.0

--- WearAppVoice/wear/src/main/java/edu/cs4730/wearappvoice/MainActivity.java
+++ WearAppVoice/wear/src/main/java/edu/cs4730/wearappvoice/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
-import android.support.wearable.view.WatchViewStub;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,28 +14,23 @@ public class MainActivity extends Activity {
 
     private TextView mTextView;
     private static final int SPEECH_REQUEST_CODE = 0;
-    Button myButton ;
+    Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-                //button to start the voice rec.
-                myButton = (Button) stub.findViewById(R.id.wrbutton);
-                myButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        displaySpeechRecognizer();
-                    }
-                });
 
+        mTextView = (TextView) findViewById(R.id.text);
+        //button to start the voice rec.
+        myButton = (Button) findViewById(R.id.wrbutton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displaySpeechRecognizer();
             }
         });
+
     }
 
 
@@ -44,19 +38,19 @@ public class MainActivity extends Activity {
     private void displaySpeechRecognizer() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-// Start the activity, the intent will be populated with the speech text
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Start the activity, the intent will be populated with the speech text
         startActivityForResult(intent, SPEECH_REQUEST_CODE);
     }
 
     // This callback is invoked when the Speech Recognizer returns.
-// This is where you process the intent and extract the speech text from the intent.
+    // This is where you process the intent and extract the speech text from the intent.
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
                                     Intent data) {
         if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
             List<String> results = data.getStringArrayListExtra(
-                    RecognizerIntent.EXTRA_RESULTS);
+                RecognizerIntent.EXTRA_RESULTS);
             String spokenText = results.get(0);
             // Do something with spokenText
             mTextView.setText(spokenText);
