f9725452c2cb83d29dc1f00e61dd928049bd08dc
Jim Ward <seker@uwyo.edu>
2018-01-03 12:32:21 -0700

    added comments and clean  up.

--- WearableDataLayer3/mobile/src/main/java/edu/cs4730/wearabledatalayer3/MainActivity.java
+++ WearableDataLayer3/mobile/src/main/java/edu/cs4730/wearabledatalayer3/MainActivity.java
@@ -1,7 +1,5 @@
 package edu.cs4730.wearabledatalayer3;
 
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +20,31 @@ import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 
+/**
+ * this is the main activity on the device/phone  (just to kept everything straight)
+ * <p>
+ * The device will setup a local listener to receive data from the wear device and display them to the screen.
+ * <p>
+ * It also setups up a button, so it can send a message to the wear device, the wear device will auto
+ * response to the message.   This code does not auto response, otherwise we would get caught in a loop.
+ * <p>
+ * This is all done via the datalayer dataClient that requires om.google.android.gms:play-services-wearable
+ * in the gradle (both wear and mobile).  Also the applicationId MUST be the same in both files as well
+ * both use a the "/message_path" to send/receive messages.
+ * <p>
+ * Note, since both the mobile and wear are looking at the same path, they will all get the data sent,
+ * since it appears to be broadcast format, not individual.
+ * <P>
+ *  To send something more then a simple string see https://github.com/googlesamples/android-DataLayer,
+ *  which sends a picture via the asset pieces.
+ * <p>
+ * debugging over bluetooth.
+ * https://developer.android.com/training/wearables/apps/debugging.html
+ * adb forward tcp:4444 localabstract:/adb-hub
+ * adb connect 127.0.0.1:4444
+ */
+
+
 public class MainActivity extends AppCompatActivity implements
     DataClient.OnDataChangedListener,
     View.OnClickListener {
@@ -30,7 +53,6 @@ public class MainActivity extends AppCompatActivity implements
     String datapath = "/data_path";
     Button mybutton;
     TextView logger;
-    //   protected Handler handler;
     String TAG = "Mobile MainActivity";
     int num = 1;
 
@@ -45,18 +67,19 @@ public class MainActivity extends AppCompatActivity implements
         logger = findViewById(R.id.logger);
     }
 
+    // add data listener
     @Override
     public void onResume() {
         super.onResume();
         Wearable.getDataClient(this).addListener(this);
     }
 
+    //remove data listener
     @Override
     public void onPause() {
         super.onPause();
         Wearable.getDataClient(this).removeListener(this);
     }
-
 
     /**
      * simple method to add the log TextView.
@@ -77,9 +100,10 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     /**
-     * Receives the data
+     * Receives the data, note since we are using the same data base, we will also receive
+     * data that we sent as well.  Likely should add some kind of id number to datamap, so it
+     * can tell between mobile device and wear device.  or this maybe the functionality you want.
      */
-
     @Override
     public void onDataChanged(@NonNull DataEventBuffer dataEventBuffer) {
         Log.d(TAG, "onDataChanged: " + dataEventBuffer);
@@ -105,7 +129,8 @@ public class MainActivity extends AppCompatActivity implements
 
 
     /**
-     * Sends the data.
+     * Sends the data.  Since it specify a client, everyone who is listening to the path, will
+     * get the data.
      */
     private void sendData(String message) {
         PutDataMapRequest dataMap = PutDataMapRequest.create(datapath);
@@ -114,13 +139,13 @@ public class MainActivity extends AppCompatActivity implements
         request.setUrgent();
 
         Task<DataItem> dataItemTask = Wearable.getDataClient(this).putDataItem(request);
-
-        dataItemTask.addOnSuccessListener(new OnSuccessListener<DataItem>() {
-            @Override
-            public void onSuccess(DataItem dataItem) {
-                Log.d(TAG, "Sending message was successful: " + dataItem);
-            }
-        })
+        dataItemTask
+            .addOnSuccessListener(new OnSuccessListener<DataItem>() {
+                @Override
+                public void onSuccess(DataItem dataItem) {
+                    Log.d(TAG, "Sending message was successful: " + dataItem);
+                }
+            })
             .addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
@@ -129,6 +154,4 @@ public class MainActivity extends AppCompatActivity implements
             })
         ;
     }
-
-
 }
--- WearableDataLayer3/wear/src/main/java/edu/cs4730/wearabledatalayer3/MainActivity.java
+++ WearableDataLayer3/wear/src/main/java/edu/cs4730/wearabledatalayer3/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.wearable.Asset;
 import com.google.android.gms.wearable.DataClient;
 import com.google.android.gms.wearable.DataEvent;
 import com.google.android.gms.wearable.DataEventBuffer;
@@ -21,7 +20,18 @@ import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 
-import java.util.Date;
+/**
+ * Wear Device code  so I can kept this straight.
+ * <p>
+ * This will receive data via the datalayer of the path listed.  Note data is a broadcast format.
+ * There is also a button to send a message to everyone, which includes itself.
+ * <p>
+ * debugging over bluetooth.
+ * https://developer.android.com/training/wearables/apps/debugging.html
+ * adb forward tcp:4444 localabstract:/adb-hub
+ * adb connect 127.0.0.1:4444
+ */
+
 
 public class MainActivity extends WearableActivity implements
     DataClient.OnDataChangedListener {
@@ -49,23 +59,25 @@ public class MainActivity extends WearableActivity implements
                 num++;
             }
         });
-
         // Enables Always-on
         setAmbientEnabled();
     }
 
+    //add listener.
     @Override
     public void onResume() {
         super.onResume();
         Wearable.getDataClient(this).addListener(this);
     }
 
+    //remove listener
     @Override
     public void onPause() {
         super.onPause();
         Wearable.getDataClient(this).removeListener(this);
     }
 
+    //receive data from the path.
     @Override
     public void onDataChanged(@NonNull DataEventBuffer dataEventBuffer) {
         Log.d(TAG, "onDataChanged: " + dataEventBuffer);
@@ -78,11 +90,7 @@ public class MainActivity extends WearableActivity implements
                     Log.v(TAG, "Wear activity received message: " + message);
                     // Display message in UI
                     mTextView.setText(message);
-                    //here, send a message back.
-                  //  message = "Hello device " + num;
-                    //Requires a new thread to avoid blocking the UI
-                  //  sendData(message);
-                  //  num++;
+
                 } else {
                     Log.e(TAG, "Unrecognized path: " + path);
                 }
@@ -95,7 +103,7 @@ public class MainActivity extends WearableActivity implements
     }
 
     /**
-     *  Sends the data.
+     * Sends the data, not this is a broadcast, so we will get the message as well.
      */
     private void sendData(String message) {
         PutDataMapRequest dataMap = PutDataMapRequest.create(datapath);
@@ -104,13 +112,13 @@ public class MainActivity extends WearableActivity implements
         request.setUrgent();
 
         Task<DataItem> dataItemTask = Wearable.getDataClient(this).putDataItem(request);
-
-        dataItemTask.addOnSuccessListener(new OnSuccessListener<DataItem>() {
-            @Override
-            public void onSuccess(DataItem dataItem) {
-                Log.d(TAG, "Sending message was successful: " + dataItem);
-            }
-        })
+        dataItemTask
+            .addOnSuccessListener(new OnSuccessListener<DataItem>() {
+                @Override
+                public void onSuccess(DataItem dataItem) {
+                    Log.d(TAG, "Sending message was successful: " + dataItem);
+                }
+            })
             .addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
