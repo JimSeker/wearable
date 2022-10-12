c1204fb157e18274393568fe3ff0d2582e523919
Jim Ward <seker@uwyo.edu>
2022-10-12 16:30:34 -0600

    updated to api 33
    not, notifications are not appearing on the wear emulator for api 30. but it does work for older wear watchs.  I come back to this later on.

--- WearNotiDemo/app/src/main/java/edu/cs4730/wearnotidemo/MainActivity.java
+++ WearNotiDemo/app/src/main/java/edu/cs4730/wearnotidemo/MainActivity.java
@@ -1,23 +1,32 @@
 package edu.cs4730.wearnotidemo;
 
+import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationCompat.BigTextStyle;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.app.NotificationCompat.WearableExtender;
 import androidx.core.app.RemoteInput;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.util.Log;
 import android.view.View;
+
+import java.util.Map;
 
 /**
  * This example shows varying notifications that work on the wearable device as well as on the
@@ -25,12 +34,20 @@ import android.view.View;
  * <p>
  * Don't forget to use this command if running an emulator for wear.  Run everytime you connect the phone.
  * adb -d forward tcp:5601 tcp:5601
+ *
+ * Note, this is not working with the new wear 3 at with the emulator.  works on the older wear watchs still
+ * missing something.
  */
 
 
 public class MainActivity extends AppCompatActivity {
     public static String id = "test_channel_01";
+    static String TAG = "MainActivity";
     int notificationID = 1;
+    NotificationManager nm;
+    ActivityResultLauncher<String[]> rpl;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{Manifest.permission.POST_NOTIFICATIONS};
+
     // Key for the string that's delivered in the action's intent
     private static final String EXTRA_VOICE_REPLY = "extra_voice_reply";
 
@@ -38,6 +55,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // for notifications permission now required in api 33
+        //this allows us to check with multiple permissions, but in this case (currently) only need 1.
+        rpl = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
+            new ActivityResultCallback<Map<String, Boolean>>() {
+                @Override
+                public void onActivityResult(Map<String, Boolean> isGranted) {
+                    boolean granted = true;
+                    for (Map.Entry<String, Boolean> x : isGranted.entrySet()) {
+                        logthis(x.getKey() + " is " + x.getValue());
+                        if (!x.getValue()) granted = false;
+                    }
+                    if (granted)
+                        logthis("Permissions granted for api 33+");
+                }
+            }
+        );
+
 
         this.findViewById(R.id.simpleButton).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +106,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         createchannel();
+        //for the new api 33+ notifications permissions.
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (!allPermissionsGranted()) {
+                rpl.launch(REQUIRED_PERMISSIONS);
+            }
+        }
     }
 
     /**
@@ -83,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
         viewIntent.putExtra("NotiID", "Notification ID is " + notificationID);
 
         PendingIntent viewPendingIntent =
-            PendingIntent.getActivity(this, 0, viewIntent, 0);
+            PendingIntent.getActivity(this, 0, viewIntent, PendingIntent.FLAG_IMMUTABLE);
 
         //Now create the notification.  We must use the NotificationCompat or it will not work on the wearable.
         NotificationCompat.Builder notificationBuilder =
@@ -107,19 +149,19 @@ public class MainActivity extends AppCompatActivity {
      * This one adds a button to the notification.  launches the camera for this example.
      */
     void addbuttonNoti() {
-        Log.i("main", "addbutton noti");
+        logthis( "addbutton noti");
         //create the intent to launch the notiactivity, then the pentingintent.
         Intent viewIntent = new Intent(this, NotiActivity.class);
         viewIntent.putExtra("NotiID", "Notification ID is " + notificationID);
 
         PendingIntent viewPendingIntent =
-            PendingIntent.getActivity(this, 0, viewIntent, 0);
+            PendingIntent.getActivity(this, 0, viewIntent, PendingIntent.FLAG_IMMUTABLE);
 
         // we are going to add an intent to open the camera here.
         //Intent cameraIntent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         PendingIntent cameraPendingIntent =
-            PendingIntent.getActivity(this, 0, cameraIntent, 0);
+            PendingIntent.getActivity(this, 0, cameraIntent, PendingIntent.FLAG_IMMUTABLE);
 
         NotificationCompat.Action.WearableExtender inlineActionForWear2 =
             new NotificationCompat.Action.WearableExtender()
@@ -165,12 +207,12 @@ public class MainActivity extends AppCompatActivity {
         viewIntent.putExtra("NotiID", "Notification ID is " + notificationID);
 
         PendingIntent viewPendingIntent =
-            PendingIntent.getActivity(this, 0, viewIntent, 0);
+            PendingIntent.getActivity(this, 0, viewIntent, PendingIntent.FLAG_IMMUTABLE);
 
         // we are going to add an intent to open the camera here.
         Intent cameraIntent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
         PendingIntent cameraPendingIntent =
-            PendingIntent.getActivity(this, 0, cameraIntent, 0);
+            PendingIntent.getActivity(this, 0, cameraIntent, PendingIntent.FLAG_IMMUTABLE);
 
         // Create the action
         NotificationCompat.Action action =
@@ -207,7 +249,7 @@ public class MainActivity extends AppCompatActivity {
         viewIntent.putExtra("NotiID", "Notification ID is " + notificationID);
 
         PendingIntent viewPendingIntent =
-            PendingIntent.getActivity(this, 0, viewIntent, 0);
+            PendingIntent.getActivity(this, 0, viewIntent, PendingIntent.FLAG_IMMUTABLE);
 
         BigTextStyle bigStyle = new NotificationCompat.BigTextStyle();
         bigStyle.bigText("Big text style.\n"
@@ -246,7 +288,7 @@ public class MainActivity extends AppCompatActivity {
         replyIntent.putExtra("NotiID", "Notification ID is " + notificationID);
 
         PendingIntent replyPendingIntent =
-            PendingIntent.getActivity(this, 0, replyIntent, 0);
+            PendingIntent.getActivity(this, 0, replyIntent, PendingIntent.FLAG_MUTABLE);
 
         // create the remote input part for the notification.
         RemoteInput remoteInput = new RemoteInput.Builder(EXTRA_VOICE_REPLY)
@@ -286,23 +328,32 @@ public class MainActivity extends AppCompatActivity {
      * for API 26+ create notification channels
      */
     private void createchannel() {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            NotificationChannel mChannel = new NotificationChannel(id,
-                getString(R.string.channel_name),  //name of the channel
-                NotificationManager.IMPORTANCE_DEFAULT);   //importance level
-            //important level: default is is high on the phone.  high is urgent on the phone.  low is medium, so none is low?
-            // Configure the notification channel.
-            mChannel.setDescription(getString(R.string.channel_description));
-            mChannel.enableLights(true);
-            //Sets the notification light color for notifications posted to this channel, if the device supports this feature.
-            mChannel.setLightColor(Color.RED);
-            mChannel.enableVibration(true);
-            mChannel.setShowBadge(true);
-            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-            nm.createNotificationChannel(mChannel);
+        NotificationChannel mChannel = new NotificationChannel(id,
+            getString(R.string.channel_name),  //name of the channel
+            NotificationManager.IMPORTANCE_DEFAULT);   //importance level
+        //important level: default is is high on the phone.  high is urgent on the phone.  low is medium, so none is low?
+        // Configure the notification channel.
+        mChannel.setDescription(getString(R.string.channel_description));
+        mChannel.enableLights(true);
+        //Sets the notification light color for notifications posted to this channel, if the device supports this feature.
+        mChannel.setLightColor(Color.RED);
+        mChannel.enableVibration(true);
+        mChannel.setShowBadge(true);
+        mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
 
-        }
+        Log.d(TAG, "channels created.");
     }
-
+    //ask for permissions when we start.
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public void logthis(String msg) {
+        //logger.append(msg);
+        Log.d(TAG, msg);
+    }
 }
